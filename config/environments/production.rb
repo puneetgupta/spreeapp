0f57a05f2d5ccc5662858a5e31ee2d23ca879f55
vishal <vsharma@enbake.com>
2014-04-25 15:41:19 +0530

    Added User confirmation emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,5 @@ SpreeStore::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = { :host => 'ec2-54-213-14-119.us-west-2.compute.amazonaws.com' }
 end
