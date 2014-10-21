c43787e304eacdf5ef5507c4456aec4bead4e206
Mayank <mmishra@enbake.com>
2014-10-21 19:56:40 +0530

    Added login authentication with place bid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+
+  #protect_from_forgery
+  def after_sign_in_path_for(resource_or_scope)
+    if session[:bid].present?
+      Bid.place_bid(session[:bid], resource_or_scope)
+      session[:bid] = nil
+      products_path
+    end
+  end
 end
