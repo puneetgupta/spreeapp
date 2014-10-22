a4338006a731dbe6480efb070b2a2f65401b5b57
Mayank <mmishra@enbake.com>
2014-10-22 16:21:47 +0530

    remove instance variable from bid model

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,8 +2,8 @@ class Bid < ActiveRecord::Base
   attr_accessible :date, :price, :variant_id, :user_id
 
   def self.place_bid(bid, user)
-    @bid = Bid.find_or_initialize_by_variant_id_and_user_id(bid.variant_id, user.id)
-    @bid.price = bid.price
-    @bid.save
+    bid_obj = Bid.find_or_initialize_by_variant_id_and_user_id(bid.variant_id, user.id)
+    bid_obj.price = bid.price
+    bid_obj.save
   end
 end
