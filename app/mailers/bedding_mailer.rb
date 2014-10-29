f4fa49acaaab41822e821bdf0026eaec2e2bb1e9
Mayank <mmishra@enbake.com>
2014-10-29 22:28:29 +0530

    change the text of mailers

--- app/mailers/bedding_mailer.rb
+++ app/mailers/bedding_mailer.rb
@@ -1,14 +1,15 @@
 class BeddingMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def bedding_mail user
-    mail(to: user.email, subject: 'Thanks for bedding in AuctionHouse')
+  def bedding_mail user,bid
+    @bid = bid
+    mail(to: user.email, subject: 'Bid Placed Successfully @AuctionHouse')
   end
 
   def winning_mail bid
     @bid = bid
     user = Spree::User.find bid.user_id
-    mail(to: user.email, subject: 'You are winner for Auction')
+    mail(to: user.email, subject: 'Congrats ! Bid Winner at AuctionHouse')
   end
 
 
